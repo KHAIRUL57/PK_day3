920dd6a833f66763a339945a9c996a82d8e7393e
KHAIRUL57 <khairultamimi5757@gmail.com>
2020-08-11 15:36:10 +0700

    learn to create javascrip

---
+++ script.js
@@ -0,0 +1,9 @@
+window.alert("hello world")
+// window.prompt("siapa anda itu")
+
+document.getElementById("Element")
+console.log(document.getElementById("element"))
+
+document.body.innerHTML += "<h1>Hello World</h1>"
+
+document.
